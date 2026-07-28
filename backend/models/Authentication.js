3ab65d0106094875f64023e344dbf5b364974b6d
Diyamittal19 <mittaldiya56@gmail.com>
2026-07-28 18:09:18 +0530

    change

--- backend/models/Authentication.js
+++ backend/models/Authentication.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const userSchema = new mongoose.Schema({
 
-    Username:{
+    username:{
         type:String,
         required:true,
         unique:true,
