69b59b08c364caf89817e48ce32db5c7e8b301f9
Diyamittal19 <mittaldiya56@gmail.com>
2026-08-01 16:53:23 +0530

    Add profile box

--- frontend/student-dashboard/js/script.js
+++ frontend/student-dashboard/js/script.js
@@ -104,3 +104,24 @@ function viewDetails(id) {
 
   window.location.href = "event.html";
 }
+
+//Profile dropdown box
+
+const profileBtn = document.getElementById("profileBtn");
+const dropdown = document.getElementById("profileDropdown");
+
+// Open/Close on Profile click
+profileBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    dropdown.classList.toggle("active");
+});
+
+// Prevent closing when clicking inside dropdown
+dropdown.addEventListener("click", function (e) {
+    e.stopPropagation();
+});
+
+// Close if user clicks anywhere else
+document.addEventListener("click", function () {
+    dropdown.classList.remove("active");
+});
