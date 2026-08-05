a66da427c64ca90356376d83459f0d42aa27ad35
Angel <angelll.jain.23@gmail.com>
2026-08-05 14:38:12 +0530

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
 const { verifyToken } = require("../middleware/auth");
-const {getEvents, getMyEvents, createEvent, updateEvent, deleteEvent, getRegistrationsForMyEvents} = require("../controllers/eventController");
+const {getEvents, getSingleEvent, getMyEvents, createEvent, updateEvent, deleteEvent, getRegistrationsForMyEvents} = require("../controllers/eventController");
 
 router.get("/", getEvents);
+router.get("/:id", getSingleEvent);
 router.get("/my", verifyToken, getMyEvents);
 router.get("/registrations/mine", verifyToken, getRegistrationsForMyEvents);
 router.post("/", verifyToken, upload.single("banner"), createEvent);
