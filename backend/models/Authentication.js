db6536c5797b6cba75b5beb44153ba3b676d7045
devika2827 <devika.aji2802@gmail.com>
2026-08-05 16:39:17 +0530

    Code cleanup

--- backend/models/Authentication.js
+++ backend/models/Authentication.js
@@ -55,7 +55,6 @@ userSchema.pre("save", async function(){
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    
 });
 userSchema.methods.isPasswordCorrect = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
--- frontend/student-dashboard/js/script.js
+++ frontend/student-dashboard/js/script.js
@@ -68,21 +68,3 @@ function viewDetails(id) {
   window.location.href = "event.html";
 }
 
-document.getElementById("logoutBtn").addEventListener("click", async () => {
-    try {
-        const response = await fetch("/api/v1/auth/logout", {
-            method: "POST",
-            credentials: "include"
-        });
-
-        if (response.ok) {
-            window.location.href = "../../Authentication/html/login.html";
-        } else {
-            const data = await response.json();
-            alert(data.message);
-        }
-    } catch (error) {
-        console.error(error);
-        alert("Logout failed.");
-    }
-});
