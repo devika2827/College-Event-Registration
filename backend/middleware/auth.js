6d3dffc797203a1391babd4dd5c61aae9ff5478b
Diyamittal19 <mittaldiya56@gmail.com>
2026-08-06 21:16:37 +0530

    change logged out message

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = async (req, res,next) => {
     const token = req.cookies?.accessToken || req.header("authorization")?.replace("Bearer ", "");
     if (!token) {
-        return res.status(401).json({ message: "Unauthorized request" });
+        return res.status(401).json({ message: "User not logged in" });
     }
     try {
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
