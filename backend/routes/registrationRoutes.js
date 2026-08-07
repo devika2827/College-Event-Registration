38c4543032573bcbba7416f98df94d29b1cef09e
Angel <angelll.jain.23@gmail.com>
2026-08-07 18:02:30 +0530

    Update registrationRoutes.js

--- backend/routes/registrationRoutes.js
+++ backend/routes/registrationRoutes.js
@@ -16,7 +16,6 @@ const {
 
 } = require("../controllers/registrationController");
 
-router.get("/my", verifyToken, getMyRegistrations);
 router.get("/", getRegistrations);
 
 router.get("/:id", getRegistration);
@@ -26,6 +25,8 @@ router.post("/", createRegistration);
 router.put("/:id", updateRegistration);
 
 router.delete("/:id", deleteRegistration);
+
+router.get("/my", verifyToken, getMyRegistrations);
 
 
 module.exports = router;
